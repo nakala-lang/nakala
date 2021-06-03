0a13b0ec4dc790f664657bb799741baacff0bb4d
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-06-03 13:03:47 -0400

    CLI: Basic formatting done
    Basic formatting is done for the CLI tool instead of just debug
    statements.

--- crates/nakala-cli/src/main.rs
+++ crates/nakala-cli/src/main.rs
@@ -3,16 +3,37 @@ use std::io::{self, Write};
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
+    let mut stderr = io::stderr();
 
     let mut input = String::new();
+    let mut env = nakala::Env::default();
 
     loop {
         write!(stdout, "-> ")?;
         stdout.flush()?;
 
         stdin.read_line(&mut input)?;
-        dbg!(&input);
+
+        match run(input.trim(), &mut env) {
+            Ok(Some(val)) => writeln!(stdout, "{}", val)?,
+            Ok(None) => {}
+            Err(msg) => writeln!(stderr, "{}", msg)?,
+        }
 
         input.clear();
+    }
+}
+
+fn run(input: &str, env: &mut nakala::Env) -> Result<Option<nakala::Val>, String> {
+    let parse = nakala::parse(input).map_err(|msg| format!("Parse error: {}", msg))?;
+
+    let evaluated = parse
+        .eval(env)
+        .map_err(|msg| format!("Evaluation error: {}", msg))?;
+
+    if evaluated == nakala::Val::Unit {
+        Ok(None)
+    } else {
+        Ok(Some(evaluated))
     }
 }
