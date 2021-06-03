4388252d7093ab9aa8054341288c92c441a6b0a8
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-06-03 17:46:22 -0400

    Lexer: Added Lexer foundation

---
+++ crates/nakala/src/lexer.rs
@@ -0,0 +1,125 @@
+use logos::Logos;
+
+#[derive(Debug, Copy, Clone, PartialEq, Logos)]
+pub(crate) enum SyntaxKind {
+    #[regex(" +")]
+    Whitespace,
+
+    #[token("fn")]
+    FnKw,
+
+    #[token("let")]
+    LetKw,
+
+    #[regex("[A-Za-z][A-Za-z0-9]+")]
+    Ident,
+
+    #[regex("[0-9]+")]
+    Number,
+
+    #[token("+")]
+    Plus,
+
+    #[token("-")]
+    Minus,
+
+    #[token("*")]
+    Star,
+
+    #[token("/")]
+    Slash,
+
+    #[token("=")]
+    Equals,
+
+    #[token("{")]
+    LBrace,
+
+    #[token("}")]
+    RBrace,
+
+    #[error]
+    Error,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn check(input: &str, kind: SyntaxKind) {
+        let mut lexer = SyntaxKind::lexer(input);
+
+        assert_eq!(lexer.next(), Some(kind));
+        assert_eq!(lexer.slice(), input);
+    }
+
+    #[test]
+    fn lex_spaces() {
+        check("   ", SyntaxKind::Whitespace);
+    }
+
+    #[test]
+    fn lex_fn_keyword() {
+        check("fn", SyntaxKind::FnKw);
+    }
+
+    #[test]
+    fn lex_let_keyword() {
+        check("let", SyntaxKind::LetKw);
+    }
+
+    #[test]
+    fn lex_alphabetic_identifier() {
+        check("abcd", SyntaxKind::Ident);
+    }
+
+    #[test]
+    fn lex_alphanumeric_identifier() {
+        check("ab123cde456", SyntaxKind::Ident);
+    }
+
+    #[test]
+    fn lex_mixed_case_identifier() {
+        check("ABCdef", SyntaxKind::Ident);
+    }
+
+    #[test]
+    fn lex_number() {
+        check("123456", SyntaxKind::Number);
+    }
+
+    #[test]
+    fn lex_plus() {
+        check("+", SyntaxKind::Plus);
+    }
+
+    #[test]
+    fn lex_minus() {
+        check("-", SyntaxKind::Minus);
+    }
+
+    #[test]
+    fn lex_star() {
+        check("*", SyntaxKind::Star);
+    }
+
+    #[test]
+    fn lex_slash() {
+        check("/", SyntaxKind::Slash);
+    }
+
+    #[test]
+    fn lex_equals() {
+        check("=", SyntaxKind::Equals);
+    }
+
+    #[test]
+    fn lex_left_brace() {
+        check("{", SyntaxKind::LBrace);
+    }
+
+    #[test]
+    fn lex_right_brace() {
+        check("}", SyntaxKind::RBrace);
+    }
+}
--- crates/nakala/src/lib.rs
+++ crates/nakala/src/lib.rs
@@ -1,7 +1,4 @@
+mod lexer;
+
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod tests {}
