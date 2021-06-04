fca87a63f50288f26d9e0cc263b7763f73a70651
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-06-03 21:26:49 -0400

    Source: Created Source interface / refactor
    Instead of having all these functions inside the Parser, we now abstract
    out the lexemes into a Source, which you can then call the necessary
    functions from to get the next Lexeme. This is much cleaner and makes us
    able to handle whitespace without needing 4 helper functions in the
    parser.

--- crates/nakala/src/parser.rs
+++ crates/nakala/src/parser.rs
@@ -1,6 +1,7 @@
 mod event;
 mod expr;
 mod sink;
+mod source;
 
 use crate::lexer::{Lexeme, Lexer, SyntaxKind};
 use crate::syntax::SyntaxNode;
@@ -8,18 +9,17 @@ use event::Event;
 use expr::expr;
 use rowan::GreenNode;
 use sink::Sink;
+use source::Source;
 
 struct Parser<'l, 'input> {
-    lexemes: &'l [Lexeme<'input>],
-    cursor: usize,
+    source: Source<'l, 'input>,
     events: Vec<Event>,
 }
 
 impl<'l, 'input> Parser<'l, 'input> {
     pub fn new(lexemes: &'l [Lexeme<'input>]) -> Self {
         Self {
-            lexemes,
-            cursor: 0,
+            source: Source::new(lexemes),
             events: Vec::new(),
         }
     }
@@ -45,14 +45,11 @@ impl<'l, 'input> Parser<'l, 'input> {
     }
 
     fn bump(&mut self) {
-        self.eat_whitespace();
+        let Lexeme { kind, text } = self.source.next_lexeme().unwrap();
 
-        let Lexeme { kind, text } = self.lexemes[self.cursor];
-
-        self.cursor += 1;
         self.events.push(Event::AddToken {
-            kind,
-            text: text.into(),
+            kind: *kind,
+            text: (*text).into(),
         });
     }
 
@@ -61,20 +58,7 @@ impl<'l, 'input> Parser<'l, 'input> {
     }
 
     fn peek(&mut self) -> Option<SyntaxKind> {
-        self.eat_whitespace();
-        self.peek_raw()
-    }
-
-    fn eat_whitespace(&mut self) {
-        while self.peek_raw() == Some(SyntaxKind::Whitespace) {
-            self.cursor += 1;
-        }
-    }
-
-    fn peek_raw(&self) -> Option<SyntaxKind> {
-        self.lexemes
-            .get(self.cursor)
-            .map(|Lexeme { kind, .. }| *kind)
+        self.source.peek_kind()
     }
 }
 
