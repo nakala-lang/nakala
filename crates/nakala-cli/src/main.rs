ec0968405fc23d2f022e7ca51501203c34c27f9e
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-06-03 20:54:39 -0400

    Parser: Rework parser to use lexemes

--- crates/nakala-cli/src/main.rs
+++ crates/nakala-cli/src/main.rs
@@ -1,4 +1,4 @@
-use nakala::parser::Parser;
+use nakala::parser::parse;
 use std::io::{self, Write};
 
 fn main() -> io::Result<()> {
@@ -13,7 +13,7 @@ fn main() -> io::Result<()> {
 
         stdin.read_line(&mut input)?;
 
-        let parse = Parser::new(&input).parse();
+        let parse = parse(&input);
         println!("{}", parse.debug_tree());
 
         input.clear();
