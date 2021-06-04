c010295fe65b456f09db7a46eec5bcec539a8897
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-06-04 19:15:20 -0400

    HIR: Use Memory Arena instead of Box for Recursively Defined Structs

--- crates/hir/src/lib.rs
+++ crates/hir/src/lib.rs
@@ -1,8 +1,14 @@
-use smol_str::SmolStr;
-use syntax::SyntaxKind;
+mod database;
+pub use database::Database;
 
-pub fn lower(ast: ast::Root) -> impl Iterator<Item = Stmt> {
-    ast.stmts().filter_map(Stmt::lower)
+use la_arena::Idx;
+use smol_str::SmolStr;
+
+pub fn lower(ast: ast::Root) -> (Database, Vec<Stmt>) {
+    let mut db = Database::default();
+    let stmts = ast.stmts().filter_map(|stmt| db.lower_stmt(stmt)).collect();
+
+    (db, stmts)
 }
 
 #[derive(Debug)]
@@ -11,82 +17,26 @@ pub enum Stmt {
     Expr(Expr),
 }
 
-impl Stmt {
-    fn lower(ast: ast::Stmt) -> Option<Self> {
-        let result = match ast {
-            ast::Stmt::VariableDef(ast) => Self::VariableDef {
-                name: ast.name()?.text().clone(),
-                value: Expr::lower(ast.value()),
-            },
-            ast::Stmt::Expr(ast) => Self::Expr(Expr::lower(Some(ast))),
-        };
-
-        Some(result)
-    }
-}
+type ExprIdx = Idx<Expr>;
 
 #[derive(Debug)]
 pub enum Expr {
     Missing,
     Binary {
         op: BinaryOp,
-        lhs: Box<Self>,
-        rhs: Box<Self>,
+        lhs: ExprIdx,
+        rhs: ExprIdx,
     },
     Literal {
         n: u64,
     },
     Unary {
         op: UnaryOp,
-        expr: Box<Self>,
+        expr: ExprIdx,
     },
     VariableRef {
         var: SmolStr,
     },
-}
-
-impl Expr {
-    fn lower(ast: Option<ast::Expr>) -> Self {
-        if let Some(ast) = ast {
-            match ast {
-                ast::Expr::BinaryExpr(ast) => Self::lower_binary(ast),
-                ast::Expr::Literal(ast) => Self::Literal { n: ast.parse() },
-                ast::Expr::ParenExpr(ast) => Expr::lower(ast.expr()),
-                ast::Expr::UnaryExpr(ast) => Self::lower_unary(ast),
-                ast::Expr::VariableRef(ast) => Self::VariableRef { var: ast.name() },
-            }
-        } else {
-            Self::Missing
-        }
-    }
-
-    fn lower_binary(ast: ast::BinaryExpr) -> Self {
-        let op = match ast.op().unwrap().kind() {
-            SyntaxKind::Plus => BinaryOp::Add,
-            SyntaxKind::Minus => BinaryOp::Sub,
-            SyntaxKind::Star => BinaryOp::Mul,
-            SyntaxKind::Slash => BinaryOp::Div,
-            _ => unreachable!(),
-        };
-
-        Self::Binary {
-            op,
-            lhs: Box::new(Expr::lower(ast.lhs())),
-            rhs: Box::new(Expr::lower(ast.rhs())),
-        }
-    }
-
-    fn lower_unary(ast: ast::UnaryExpr) -> Self {
-        let op = match ast.op().unwrap().kind() {
-            SyntaxKind::Minus => UnaryOp::Neg,
-            _ => unreachable!(),
-        };
-
-        Self::Unary {
-            op,
-            expr: Box::new(Expr::lower(ast.expr())),
-        }
-    }
 }
 
 #[derive(Debug)]
