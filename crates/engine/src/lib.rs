28518256034673637d58e2a9cd75bbc656c02791
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-06-05 11:13:25 -0400

    CLI: Lots of REPL features
    Prompt with basic printing features added as well as help message and
    version message

--- crates/engine/src/lib.rs
+++ crates/engine/src/lib.rs
@@ -1,7 +1,18 @@
 use hir::{Database, Stmt};
 
 pub mod val;
+use val::Val;
 
-pub fn eval(hir: (Database, Vec<Stmt>)) -> val::Val {
+pub fn eval(hir: (Database, Vec<Stmt>)) -> Val {
+    todo!();
+}
+
+fn eval_stmt(stmt: Stmt) -> Val {
     todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn eval_add() {}
 }
--- crates/nakala/src/main.rs
+++ crates/nakala/src/main.rs
@@ -1,30 +1,171 @@
+use crossterm::cursor::{MoveLeft, MoveToColumn};
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers, MouseEvent};
+use crossterm::style::{style, Attribute, Color, Print, PrintStyledContent};
+use crossterm::QueueableCommand;
+use crossterm::Result;
 use parser::parse;
-use std::io::{self, Write};
+use std::io::{self, Stdout, Write};
 
-fn main() -> io::Result<()> {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-
-    let mut input = String::new();
+fn main() -> Result<()> {
+    let mut le = LineEditor::new("> ")?;
+    crossterm::terminal::enable_raw_mode()?;
 
     loop {
-        write!(stdout, "-> ")?;
-        stdout.flush()?;
-
-        stdin.read_line(&mut input)?;
-
-        let parse = parse(&input);
-        println!("{}", parse.debug_tree());
-
-        let mut ast_tree = ast::Root::cast(parse.syntax()).unwrap();
-        ast_tree = dbg!(ast_tree);
-
-        let mut hir = hir::lower(ast_tree);
-        hir = dbg!(hir);
-
-        let result = engine::eval(hir);
-        println!("{:?}", result);
-
-        input.clear();
+        match read()? {
+            Event::Key(key_event) => le.dispatch_key_event(key_event)?,
+            Event::Mouse(mouse_event) => le.dispatch_mouse_event(mouse_event)?,
+            Event::Resize(new_cols, new_rows) => le.dispatch_resize_event(new_cols, new_rows)?,
+        }
     }
 }
+
+pub struct LineEditor {
+    stdout: Stdout,
+    buffer: String,
+    prompt: &'static str,
+}
+
+impl LineEditor {
+    pub fn new(prompt: &'static str) -> Result<Self> {
+        let mut le = Self {
+            stdout: io::stdout(),
+            buffer: String::new(),
+            prompt,
+        };
+
+        le.print_version_message()?;
+        le.reset_prompt()?;
+
+        Ok(le)
+    }
+
+    pub fn dispatch_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
+        if key_event == KILL_KEY_EVENT {
+            std::process::exit(0);
+        }
+
+        let KeyEvent { code, .. } = key_event;
+        match code {
+            KeyCode::Char(c) => {
+                self.buffer.push(c);
+                self.stdout.queue(Print(c))?.flush()?;
+            }
+            KeyCode::Backspace => {
+                if !self.buffer.is_empty() {
+                    self.buffer.pop();
+
+                    self.stdout
+                        .queue(MoveLeft(1))?
+                        .queue(Print(' '))?
+                        .queue(MoveLeft(1))?
+                        .flush()?;
+                }
+            }
+            KeyCode::Enter => {
+                self.new_line()?;
+                self.execute_buffer()?;
+                self.reset_prompt()?;
+            }
+
+            _ => {}
+        }
+
+        Ok(())
+    }
+
+    #[allow(unused_variables)]
+    pub fn dispatch_mouse_event(&mut self, mouse_event: MouseEvent) -> Result<()> {
+        Ok(())
+    }
+
+    #[allow(unused_variables)]
+    pub fn dispatch_resize_event(&mut self, new_cols: u16, new_rows: u16) -> Result<()> {
+        Ok(())
+    }
+
+    fn execute_buffer(&mut self) -> Result<()> {
+        match self.buffer.as_str() {
+            "help" => self.print_help_message()?,
+            "version" => self.print_version_message()?,
+            _ => self.parse_buffer(),
+        }
+
+        self.buffer.clear();
+
+        Ok(())
+    }
+
+    fn parse_buffer(&mut self) {
+        let parse = parse(&self.buffer);
+        //self.stdout.write_all(parse.debug_tree().as_bytes());
+
+        let ast_tree = ast::Root::cast(parse.syntax()).unwrap();
+        //ast_tree = dbg!(ast_tree);
+
+        let hir = hir::lower(ast_tree);
+        self.stdout
+            .queue(Print(format!("{:?}", hir.1)))
+            .unwrap()
+            .flush()
+            .unwrap();
+
+        // let result = engine::eval(hir);
+        // println!("{:?}", result);
+    }
+
+    fn new_line(&mut self) -> Result<()> {
+        self.stdout
+            .queue(Print('\n'))?
+            .queue(MoveToColumn(0))?
+            .flush()?;
+
+        Ok(())
+    }
+
+    fn reset_prompt(&mut self) -> Result<()> {
+        self.new_line()?;
+        self.stdout.queue(Print(self.prompt))?.flush()?;
+
+        Ok(())
+    }
+
+    fn print_version_message(&mut self) -> Result<()> {
+        let header_msg = "Nakala v0.1.0\n";
+        let body_msg = "Type \"help\" for more information";
+
+        let styled_header = style(header_msg)
+            .with(Color::DarkYellow)
+            .attribute(Attribute::Bold);
+        self.stdout
+            .queue(PrintStyledContent(styled_header))?
+            .queue(MoveToColumn(0))?
+            .queue(Print(body_msg))?
+            .flush()?;
+
+        Ok(())
+    }
+
+    fn print_help_message(&mut self) -> Result<()> {
+        let title = "Nakala";
+        let header_msg = " - a hobby programming language based on azrg's Eldiro blog posts.\n";
+        let body_msg = "For more information, visit https://github.com/reaganmcf/nakala";
+
+        let styled_title = style(title)
+            .with(Color::DarkYellow)
+            .attribute(Attribute::Bold);
+
+        self.stdout
+            .queue(PrintStyledContent(styled_title))?
+            .queue(Print(header_msg))?
+            .queue(MoveToColumn(0))?
+            .queue(Print(body_msg))?
+            .flush()?;
+
+        Ok(())
+    }
+}
+
+const KILL_KEY_EVENT: KeyEvent = KeyEvent {
+    code: KeyCode::Char('c'),
+    modifiers: KeyModifiers::CONTROL,
+};
