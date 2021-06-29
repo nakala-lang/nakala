52af9182ebf1a841bdddd31ff03178bb0785a85f
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-06-29 08:19:37 -0400

    Engine: Add env to store variable references
    Env is a data structure that stores bindings, allowing expressions to
    look up previously defined bindings in expressions. Note that this is
    not implemented yet, and this patch just introduces the data structure
    that is going to be used in the main implementation

---
+++ crates/engine/src/env.rs
@@ -0,0 +1,35 @@
+use super::EngineError;
+use crate::val::Val;
+use std::collections::HashMap;
+
+pub struct Env {
+    // holds variable definitions
+    bindings: HashMap<String, Val>,
+}
+
+impl Env {
+    pub fn get_binding(&self, binding_name: String) -> Result<Val, EngineError> {
+        match self.bindings.get(&binding_name) {
+            Some(val) => Ok(val.to_owned()),
+            None => Err(EngineError::BindingUndefined { binding_name }),
+        }
+    }
+
+    pub fn set_binding(&mut self, binding_name: String, val: Val) -> Result<Val, EngineError> {
+        if self.bindings.contains_key(&binding_name) {
+            return Err(EngineError::BindingAlreadyExists { binding_name });
+        }
+
+        self.bindings.insert(binding_name, val);
+
+        Ok(Val::Unit)
+    }
+}
+
+impl std::default::Default for Env {
+    fn default() -> Self {
+        Self {
+            bindings: HashMap::default(),
+        }
+    }
+}
