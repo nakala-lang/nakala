a83b4027de4fbee92ae8d24b2c1d08b0382b4adc
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-07-03 12:05:52 -0400

    CLI: use extracted line editor and pub parse_buff
    The logic now uses the extracted line editor instead of having it inside
    the main file. Also, parse_and_eval_buffer is now a pub extracted
    function which means we can easily parse and eval without needing the
    line editor involved.

--- crates/nakala/src/main.rs
+++ crates/nakala/src/main.rs
@@ -1,12 +1,15 @@
+mod line_editor;
+
 use clap::{App, Arg, ArgMatches};
-use crossterm::cursor::{MoveLeft, MoveToColumn};
-use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers, MouseEvent};
-use crossterm::style::{style, Attribute, Color, Print, PrintStyledContent};
-use crossterm::QueueableCommand;
+use crossterm::event::{read, Event};
 use crossterm::Result;
 use engine::env::Env;
-use parser::parse;
-use std::io::{self, Stdout, Write};
+use engine::error::EngineError;
+use engine::val::Val;
+use hir::Hir;
+use line_editor::LineEditor;
+use parser::Parse;
+use std::fs::read_to_string;
 use std::path::Path;
 
 fn main() {
@@ -37,14 +40,26 @@ fn main() {
     if let Some(path) = matches.value_of("file") {
         let path = Path::new(path);
         if path.exists() {
-            unimplemented!("Not yet implemented")
+            // parse the entire file into a buffer and execute it
+            match read_to_string(path) {
+                Ok(buf) => {
+                    parse_and_eval_buffer(&buf, &mut Env::default());
+                }
+                Err(_) => {
+                    eprintln!("Failed to parse file.");
+                }
+            }
         } else {
             eprintln!("File does not exist.");
         }
     } else {
+        // Load line editor
         match cli_main(matches) {
             Ok(_) => {}
-            Err(_) => eprintln!("An error occurred."),
+            Err(_) => {
+                eprintln!("An error occurred.");
+                crossterm::terminal::disable_raw_mode().unwrap();
+            }
         }
     }
 }
@@ -62,205 +77,19 @@ fn cli_main(cli_args: ArgMatches) -> Result<()> {
     }
 }
 
-pub struct LineEditor {
-    stdout: Stdout,
-    buffer: String,
-    env: Env,
-    prompt: &'static str,
-    show_parse: bool,
-    show_hir: bool,
+pub struct NakalaResult {
+    parse: Parse,
+    hir: Hir,
+    val: Val,
 }
 
-impl LineEditor {
-    pub fn new(prompt: &'static str, cli_args: ArgMatches) -> Result<Self> {
-        let mut le = Self {
-            stdout: io::stdout(),
-            buffer: String::new(),
-            env: Env::default(),
-            prompt,
-            show_parse: cli_args.is_present("parse"),
-            show_hir: cli_args.is_present("hir"),
-        };
+pub fn parse_and_eval_buffer(
+    buffer: &String,
+    env: &mut Env,
+) -> std::result::Result<NakalaResult, EngineError> {
+    let parse = parser::parse(buffer.as_str());
+    let ast_tree = ast::Root::cast(parse.syntax()).unwrap();
+    let hir = hir::lower(ast_tree);
 
-        le.print_version_message()?;
-        le.reset_prompt()?;
-
-        Ok(le)
-    }
-
-    pub fn dispatch_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
-        if key_event == KILL_KEY_EVENT {
-            self.exit().expect("Failed to exit");
-        }
-
-        let KeyEvent { code, .. } = key_event;
-        match code {
-            KeyCode::Char(c) => {
-                self.buffer.push(c);
-                self.stdout.queue(Print(c))?.flush()?;
-            }
-            KeyCode::Backspace => {
-                if !self.buffer.is_empty() {
-                    self.buffer.pop();
-
-                    self.stdout
-                        .queue(MoveLeft(1))?
-                        .queue(Print(' '))?
-                        .queue(MoveLeft(1))?
-                        .flush()?;
-                }
-            }
-            KeyCode::Enter => {
-                self.new_line()?;
-                self.execute_buffer()?;
-                self.reset_prompt()?;
-            }
-
-            _ => {}
-        }
-
-        Ok(())
-    }
-
-    #[allow(unused_variables)]
-    pub fn dispatch_mouse_event(&mut self, mouse_event: MouseEvent) -> Result<()> {
-        Ok(())
-    }
-
-    #[allow(unused_variables)]
-    pub fn dispatch_resize_event(&mut self, new_cols: u16, new_rows: u16) -> Result<()> {
-        Ok(())
-    }
-
-    fn execute_buffer(&mut self) -> Result<()> {
-        match self.buffer.as_str() {
-            "help" => self.print_help_message()?,
-            "version" => self.print_version_message()?,
-            "__env" => self.print_env()?,
-            _ => self.parse_buffer(),
-        }
-
-        self.buffer.clear();
-
-        Ok(())
-    }
-
-    fn print_env(&mut self) -> Result<()> {
-        let mut env_str = String::new();
-        for (k, v) in self.env.get_all_bindings() {
-            env_str.push_str(format!("`{:?}`: `{:?}`\n", k, v).as_str());
-        }
-
-        self.print_big_string(env_str)
-    }
-
-    fn parse_buffer(&mut self) {
-        let parse = parse(&self.buffer);
-        if self.show_parse {
-            self.print_big_string(parse.debug_tree())
-                .expect("Failed to print big string");
-        }
-
-        let ast_tree = ast::Root::cast(parse.syntax()).unwrap();
-
-        let hir = hir::lower(ast_tree);
-        if self.show_hir {
-            self.print_big_string(format!("{:#?}", hir.1))
-                .expect("Failed to print big string");
-        }
-
-        let engine_result = engine::eval(&mut self.env, hir);
-        match engine_result {
-            Ok(res) => {
-                println!(" {}", res);
-            }
-            Err(err) => {
-                self.print_error(Box::new(err)).unwrap();
-            }
-        };
-    }
-
-    fn print_error(&mut self, err: Box<dyn std::error::Error>) -> Result<()> {
-        let error_header = style("ERROR: ").with(Color::Red).attribute(Attribute::Bold);
-        self.stdout
-            .queue(PrintStyledContent(error_header))?
-            .queue(Print(err.to_string()))?
-            .flush()?;
-
-        self.new_line()?;
-
-        Ok(())
-    }
-
-    fn new_line(&mut self) -> Result<()> {
-        self.stdout
-            .queue(Print('\n'))?
-            .queue(MoveToColumn(0))?
-            .flush()?;
-
-        Ok(())
-    }
-
-    fn reset_prompt(&mut self) -> Result<()> {
-        self.new_line()?;
-        self.stdout.queue(Print(self.prompt))?.flush()?;
-
-        Ok(())
-    }
-
-    fn print_version_message(&mut self) -> Result<()> {
-        let header_msg = "Nakala v0.1.0\n";
-        let body_msg = "Type \"help\" for more information";
-
-        let styled_header = style(header_msg)
-            .with(Color::DarkYellow)
-            .attribute(Attribute::Bold);
-        self.stdout
-            .queue(PrintStyledContent(styled_header))?
-            .queue(MoveToColumn(0))?
-            .queue(Print(body_msg))?
-            .flush()?;
-
-        Ok(())
-    }
-
-    fn print_help_message(&mut self) -> Result<()> {
-        let title = "Nakala";
-        let header_msg = " - a hobby programming language based on azrg's Eldiro blog posts.\n";
-        let body_msg = "For more information, visit https://github.com/reaganmcf/nakala";
-
-        let styled_title = style(title)
-            .with(Color::DarkYellow)
-            .attribute(Attribute::Bold);
-
-        self.stdout
-            .queue(PrintStyledContent(styled_title))?
-            .queue(Print(header_msg))?
-            .queue(MoveToColumn(0))?
-            .queue(Print(body_msg))?
-            .flush()?;
-
-        Ok(())
-    }
-
-    #[allow(dead_code)]
-    fn print_big_string(&mut self, big_string: String) -> Result<()> {
-        for chunk in big_string.split('\n') {
-            self.stdout.queue(Print(chunk))?;
-            self.new_line()?;
-        }
-
-        Ok(())
-    }
-
-    fn exit(&self) -> Result<()> {
-        // Cleanup
-        crossterm::terminal::disable_raw_mode()?;
-        std::process::exit(0);
-    }
+    engine::eval(env, hir.clone()).map(|val| NakalaResult { parse, hir, val })
 }
-
-const KILL_KEY_EVENT: KeyEvent = KeyEvent {
-    code: KeyCode::Char('c'),
-    modifiers: KeyModifiers::CONTROL,
-};
