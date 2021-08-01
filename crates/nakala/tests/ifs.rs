c50fe0aa55ed4b0253cf24253d90f455e4bcf35b
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-08-01 16:28:45 -0400

    Tests: Add else/elseif test cases

--- crates/nakala/tests/ifs.rs
+++ crates/nakala/tests/ifs.rs
@@ -48,3 +48,103 @@ fn complicated_if() {
     ";
     utils::compare_output(vec![], Some(input), "1");
 }
+
+#[test]
+fn if_true_with_else_doesnt_branch() {
+    let input = "
+        let x = 1
+
+        if true {
+            x = 2
+        } else {
+            x = 10000
+        }
+
+        x
+    ";
+    utils::compare_output(vec![], Some(input), "2");
+}
+
+#[test]
+fn if_false_with_else_does_branch() {
+    let input = "
+        let x = 1
+
+        if false {
+            x = 2
+        } else {
+            x = 3
+        }
+
+        x
+    ";
+    utils::compare_output(vec![], Some(input), "3");
+}
+
+#[test]
+fn if_false_with_else_if_true_does_branch() {
+    let input = "
+        let x = 1
+
+        if false {
+            x = 2
+        } else if true{
+            x = 3
+        }
+
+        x
+    ";
+    utils::compare_output(vec![], Some(input), "3");
+}
+
+#[test]
+fn if_false_with_else_if_false_doesnt_branch() {
+    let input = "
+        let x = 1
+
+        if false {
+            x = 2
+        } else if false{
+            x = 3
+        }
+
+        x
+    ";
+    utils::compare_output(vec![], Some(input), "1");
+}
+
+#[test]
+fn if_false_with_else_if_false_with_else_does_branch() {
+    let input = "
+        let x = 1
+
+        if false {
+            x = 2
+        } else if false{
+            x = 3
+        } else {
+            x = 4
+        }
+
+        x
+    ";
+    utils::compare_output(vec![], Some(input), "4");
+}
+
+#[test]
+fn if_false_with_else_if_true_with_else_branches_in_the_middle() {
+    let input = "
+        let x = 1
+
+        if false {
+            x = 2
+        } else if true {
+            x = 3
+        } else {
+            x = 4
+        }
+
+        x
+    ";
+    utils::compare_output(vec![], Some(input), "3");
+}
