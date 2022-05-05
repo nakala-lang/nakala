457210555026f170b1689fd3c75439879f1b743c
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2022-05-04 20:55:06 -0400

    delete: start rewrite

--- crates/lexer/src/lib.rs
+++ crates/lexer/src/lib.rs
@@ -1,45 +1,8 @@
-use logos::Logos;
-use std::convert::TryFrom;
-use std::ops::Range as StdRange;
-use text_size::{TextRange, TextSize};
-
-mod token_kind;
-pub use token_kind::TokenKind;
-
-pub struct Lexer<'a> {
-    inner: logos::Lexer<'a, TokenKind>,
-}
-
-impl<'a> Lexer<'a> {
-    pub fn new(input: &'a str) -> Self {
-        Self {
-            inner: TokenKind::lexer(input),
-        }
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
     }
-}
-
-impl<'a> Iterator for Lexer<'a> {
-    type Item = Token<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let kind = self.inner.next()?;
-        let text = self.inner.slice();
-
-        let range = {
-            let StdRange { start, end } = self.inner.span();
-            let start = TextSize::try_from(start).unwrap();
-            let end = TextSize::try_from(end).unwrap();
-
-            TextRange::new(start, end)
-        };
-
-        Some(Self::Item { kind, text, range })
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Token<'a> {
-    pub kind: TokenKind,
-    pub text: &'a str,
-    pub range: TextRange,
 }
