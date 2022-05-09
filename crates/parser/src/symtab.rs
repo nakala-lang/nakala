39cf3cee63937d1734c5267c8fd0199c5563c4de
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2022-05-09 19:22:52 -0400

    add: symtab data for type checking

---
+++ crates/parser/src/symtab.rs
@@ -0,0 +1,61 @@
+use ast::ty::Type;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Symbol {
+    pub name: String,
+    pub ty: Type,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct SymbolTable {
+    inner: Vec<HashMap<String, Symbol>>,
+    level: usize,
+}
+
+impl SymbolTable {
+    pub fn new() -> Self {
+        SymbolTable {
+            inner: vec![HashMap::default()],
+            level: 0,
+        }
+    }
+
+    pub fn level_up(&mut self) {
+        self.inner.push(HashMap::default());
+        self.level = self.level + 1;
+    }
+
+    pub fn level_down(&mut self) {
+        if self.level == 0 {
+            unreachable!("ICE: symtab can't go below level 0!");
+        }
+
+        self.inner.pop();
+        self.level = self.level - 1;
+    }
+
+    pub fn insert(&mut self, name: String, ty: Type) {
+        if let Some(map) = self.inner.get_mut(self.level) {
+            map.insert(name.clone(), Symbol { name, ty });
+        } else {
+            panic!("ICE: symtab is out of sync. Trying to insert into level that doesn't exist.");
+        }
+    }
+
+    pub fn lookup(&self, name: &String) -> Option<&Symbol> {
+        if let Some(map) = self.inner.get(self.level) {
+            map.get(name)
+        } else {
+            panic!("ICE: symtab is out of sync. Trying to lookup into level that doesn't exist.");
+        }
+    }
+    
+    pub fn lookup_mut(&mut self, name: &String) -> Option<&mut Symbol> {
+        if let Some(map) = self.inner.get_mut(self.level) {
+            map.get_mut(name)
+        } else {
+            panic!("ICE: symtab is out of sync. Trying to lookup into level that doesn't exist.");
+        }
+    }
+}
