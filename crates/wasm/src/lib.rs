19603877ba6993501c730b5ff5e39343c134e952
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2022-07-04 21:04:58 -0400

    fix: remove unsafe and print errors out

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,30 +1,32 @@
 use ast::ty::Type;
-use interpreter::{Builtin, Val, Value, env::Environment, error::RuntimeError, interpret};
+use interpreter::{env::Environment, error::RuntimeError, interpret, Builtin, Val, Value};
 use parser::{parse, source::Source, SymbolTable};
 use wasm_bindgen::{prelude::*, JsValue};
 
-static mut OUTPUT: String = String::new();
-
 #[wasm_bindgen]
 pub fn wasm_interpret(source: &str) -> JsValue {
-    // reset print output
-    unsafe {
-        OUTPUT = String::new();
+    match helper(source) {
+        Ok(val) => JsValue::null(),
+        Err(e) => {
+            output.push_str(&format!("{:?}", e));
+            JsValue::from_str(&output)
+        },
     }
+}
+
+pub fn helper(source: &str) -> miette::Result<()> {
+    let mut output = String::new();
 
     let mut builtins = vec![];
 
     fn print(vals: Vec<Value>, env: &mut Environment) -> Result<Value, RuntimeError> {
-        // SAFETY: OUTPUT is only every appended to so we're fine here
-        unsafe {
-            OUTPUT.push_str(&format!(
-                "{}",
-                vals.first()
-                    .expect("arity mismatch didn't catch builtin")
-                    .to_string(env)
-            ));
-            OUTPUT.push_str("\n");
-        }
+        output.push_str(&format!(
+            "{}",
+            vals.first()
+                .expect("arity mismatch didn't catch builtin")
+                .to_string(env)
+        ));
+        output.push_str("\n");
         Ok(Value::null())
     }
     builtins.push(Builtin::new(
@@ -59,21 +61,10 @@ pub fn wasm_interpret(source: &str) -> JsValue {
     let symtab = SymbolTable::new(symbols);
 
     let source = Source::new(0, String::from(source), "stdin".to_string());
-    match parse(source.clone(), symtab).map_err(|error| error.with_source_code(source.clone())) {
-        Ok(parse) => {
-            if let Err(e) =
-                interpret(parse, &mut env).map_err(|error| error.with_source_code(source.clone()))
-            {
-                unsafe {
-                    OUTPUT.push('\n');
-                    OUTPUT.push_str(&format!("{}", e));
-                }
-            }
-        }
-        Err(e) => unsafe {
-            OUTPUT.push('\n');
-            OUTPUT.push_str(&format!("{}", e));
-        },
-    }
-    unsafe { JsValue::from_str(&OUTPUT) }
+    let parse =
+        parse(source.clone(), symtab).map_err(|error| error.with_source_code(source.clone()))?;
+
+    interpret(parse, &mut env).map_err(|error| error.with_source_code(source.clone()))?;
+
+    Ok(())
 }
